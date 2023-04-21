5fe818d3579c0a8be59e79f0dac3ca9d3c43f8d0
shwetahardwin <99962470+shwetahardwin@users.noreply.github.com>
2023-04-21 23:22:25 +0530

    Initial commit

---
+++ demoFramework/demoFramework.h
@@ -0,0 +1,18 @@
+//
+//  demoFramework.h
+//  demoFramework
+//
+//  Created by Macbook on 20/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for demoFramework.
+FOUNDATION_EXPORT double demoFrameworkVersionNumber;
+
+//! Project version string for demoFramework.
+FOUNDATION_EXPORT const unsigned char demoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <demoFramework/PublicHeader.h>
+
+
